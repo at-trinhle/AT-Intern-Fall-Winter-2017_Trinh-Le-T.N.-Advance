bbb905b9f291d90cb08b864a773476fdfd72cade
Trinh Le T.N <trinh.le@asiantech.vn>
2018-01-12 17:37:22 +0700

    update UI material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,28 @@ import { AppComponent } from './app.component';
 import { HellowordComponent} from './helloword/helloword.component';
 import { HighlightDirective } from './highlight/highlight.directive';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent} from './sidebar/sidebar.component';
-import { MainComponent} from './main/main.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [ 
-    HellowordComponent, AppComponent, HighlightDirective, HeaderComponent,SidebarComponent,MainComponent
+    HellowordComponent, 
+    AppComponent, 
+    HighlightDirective, 
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
